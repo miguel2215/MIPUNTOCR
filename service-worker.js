bfeaa7564fe8711298121c55606d2a43d03f5172
miguel2215 <41209810+miguel2215@users.noreply.github.com>
2026-09-17 16:18:17 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mipuntocr-v6-oracle-cards';
+const CACHE = 'mipuntocr-v7-restaurante-mesas-cierre';
 const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
