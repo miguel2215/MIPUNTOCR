4d33b865c0c20313830c88f28a1701bd2b3d6149
miguel2215 <41209810+miguel2215@users.noreply.github.com>
2026-09-17 17:20:02 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mipuntocr-v7-2-pedidos-mesa';
+const CACHE = 'mipuntocr-v7-3-pedidos-flotantes';
 const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
